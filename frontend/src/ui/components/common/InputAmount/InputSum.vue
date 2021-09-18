7921f0c2c5e2ff3de1445dfa8a11d44d930d8247
Bro Bobro <andycyberries@gmail.com>
2021-09-18 07:26:50 +0300

    input filter

--- frontend/src/ui/components/common/InputAmount/InputSum.vue
+++ frontend/src/ui/components/common/InputAmount/InputSum.vue
@@ -1,8 +1,16 @@
 <template>
-  <input class="input-sum" type="text" :value="value" @input="onInput" />
+  <input
+    class="input-sum"
+    type="text"
+    :value="value"
+    @input="onInput"
+    @keydown="onKeydown"
+  />
 </template>
 
 <script>
+const controlChars = ["a", "c", "x", "v", "r"];
+
 export default {
   name: "InputSum",
   prop: ["value"],
@@ -12,22 +20,26 @@ export default {
       default: 0,
     },
   },
-  data() {
-    return {
-      val: this.value,
-      allowedKeys: [
-        ...new Array(10).fill(0).map((item, num) => num.toString()),
-        "Backspace",
-        "ArrowRight",
-        "ArrowLeft",
-        "Delete",
-        "Tab",
-      ],
-    };
-  },
   methods: {
     onInput(e) {
-      this.$emit("input", +e.target.value);
+      this.$emit("input", e.target.value);
+    },
+    onKeydown(e) {
+      if (this.shouldPrevent(e)) {
+        e.preventDefault();
+      }
+    },
+    shouldPrevent(e) {
+      return !(/^[0-9]*$/g.test(e.key) || this.isControlling(e));
+    },
+    isControlling(e) {
+      if (controlChars.includes(e.key.toLowerCase()) && e.ctrlKey) {
+        return true;
+      }
+
+      if (e.key === "Backspace" || e.key === "Delete") {
+        return true;
+      }
     },
   },
 };
